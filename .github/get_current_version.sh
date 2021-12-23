d84dd501a260a2c20b0857d1683246218389ec84
Jeroen Bos <jer123bos@gmail.com>
2021-12-23 16:05:48 +0100

    I think I found it now

--- .github/get_current_version.sh
+++ .github/get_current_version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-version=$(cat ./JBSnorro/JBSnorro.csproj | grep "<Version>" | sed 's/<Version>//' | sed 's/<\/Version>//' | xargs)
-# xargs trims
+version=$(cat ./JBSnorro/JBSnorro.csproj | grep "<Version>" | sed 's/<Version>//' | sed 's/<\/Version>//' | xargs | tr -d '\n')
+# xargs trims. tr trims newlines
 
 if [ -z $version ]; then
     echo "No version found"
--- .github/get_published_version.sh
+++ .github/get_published_version.sh
@@ -4,8 +4,8 @@
 # nuget list "id:JBSnorro" # doesn't hang
 # $(nuget list "id:JBSnorro") # hangs 🤷‍ in VS wsl in Developer Powershell window (but not in VSCode terminal)
 
-version=$(nuget list "id:JBSnorro" | sed 's/JBSnorro//' | xargs)
-# xargs trims
+version=$(nuget list "id:JBSnorro" | sed 's/JBSnorro//' | xargs | tr -d '\n')
+# xargs trims. tr trims newlines
 
 if [ -z $version ]; then
     echo "fatal: No version found"
