b987b49f67a53446eccd50d5b795c195a63effd2
Jeroen Bos <jer123bos@gmail.com>
2022-01-09 18:34:01 +0100

    Add SubstringUntilLast

---
+++ JBSnorro.Tests/Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using JBSnorro.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JBSnorro.Tests.Extensions
+{
+    [TestClass]
+    internal class StringExtensions
+    {
+        [TestMethod]
+        public void SubstringUntilLast_Returns_Input_If_Not_Found()
+        {
+            Assert.AreEqual("asdf", "asdf".SubstringUntilLast("t"));
+        }
+        [TestMethod]
+        public void SubstringUntilLast_Returns_Until_Last_Input_If_Found()
+        {
+            Assert.AreEqual("abcd.efgh", "abcd.efgh.ijkl".SubstringUntilLast("."));
+        }
+        [TestMethod]
+        public void SubstringUntilLast_Returns_Until_Last_Contiguous_Match()
+        {
+            Assert.AreEqual("AAAAAAAA", "AAAAAAAAAA".SubstringUntilLast("AA"));
+        }
+    }
+}
