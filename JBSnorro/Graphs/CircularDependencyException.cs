426c81bf3cc7a5c3b575ca1f3bc45f7cd4b1ba86
Jeroen Bos <jer123bos@gmail.com>
2022-01-18 09:33:02 +0100

    Make public in new file

---
+++ JBSnorro/Graphs/CircularDependencyException.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+using System;
+
+namespace JBSnorro.Graphs
+{
+    public class CircularDependencyException : Exception
+    {
+        public CircularDependencyException() { }
+        public CircularDependencyException(string? message) : base(message) { }
+        public CircularDependencyException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
+
--- JBSnorro/Graphs/CircularDependencyTracker.cs
+++ JBSnorro/Graphs/CircularDependencyTracker.cs
@@ -160,12 +160,5 @@ namespace JBSnorro.Graphs
 
         }
     }
-    
-    class CircularDependencyException : Exception
-    {
-        public CircularDependencyException() { }
-        public CircularDependencyException(string? message) : base(message) { }
-        public CircularDependencyException(string? message, Exception? innerException) : base(message, innerException) { }
-    }
 }
 
