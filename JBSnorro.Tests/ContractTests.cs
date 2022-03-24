58642c9d4cddeabe818b54cdf00180261c963423
Jeroen Bos <jer123bos@gmail.com>
2022-03-24 10:33:34 +0100

    Add more tests

--- JBSnorro.Tests/ContractTests.cs
+++ JBSnorro.Tests/ContractTests.cs
@@ -13,7 +13,22 @@ namespace JBSnorro
     public class ContractTests
     {
         [TestMethod]
-        public void TestCallerExpression()
+        public void TestCallerExpressionOnRequires()
+        {
+            const bool f = false;
+            try
+            {
+                Contract.Requires(f);
+            }
+            catch (Exception ex)
+            {
+                Contract.Assert(ex.Message == "Precondition failed: 'f'");
+                return;
+            }
+            Contract.Throw();
+        }
+        [TestMethod]
+        public void TestCallerExpressionOnAssert()
         {
             const bool f = false;
             try
@@ -23,6 +38,21 @@ namespace JBSnorro
             catch (Exception ex)
             {
                 Contract.Assert(ex.Message == "Assertion failed: 'f'");
+                return;
+            }
+            Contract.Throw();
+        }
+        [TestMethod]
+        public void TestCallerExpressionOnEnsures()
+        {
+            const bool f = false;
+            try
+            {
+                Contract.Ensures(f);
+            }
+            catch (Exception ex)
+            {
+                Contract.Assert(ex.Message == "Postcondition failed: 'f'");
                 return;
             }
             Contract.Throw();
