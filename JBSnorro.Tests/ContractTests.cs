6804ab5cf2e43338bd07bee288e572592c021253
Jeroen Bos <jer123bos@gmail.com>
2022-03-25 09:59:27 +0100

    Refactor

--- JBSnorro.Tests/ContractTests.cs
+++ JBSnorro.Tests/ContractTests.cs
@@ -7,55 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JBSnorro
+namespace JBSnorro.Tests;
+
+
+[TestClass]
+public class ContractTests
 {
-    [TestClass]
-    public class ContractTests
+    [TestMethod]
+    public void TestCallerExpressionOnRequires()
     {
-        [TestMethod]
-        public void TestCallerExpressionOnRequires()
+        const bool f = false;
+        try
         {
-            const bool f = false;
-            try
-            {
-                Contract.Requires(f);
-            }
-            catch (Exception ex)
-            {
-                Contract.Assert(ex.Message == "Precondition failed: 'f'");
-                return;
-            }
-            Contract.Throw();
+            Contract.Requires(f);
         }
-        [TestMethod]
-        public void TestCallerExpressionOnAssert()
+        catch (Exception ex)
         {
-            const bool f = false;
-            try
-            {
-                Contract.Assert(f);
-            }
-            catch (Exception ex)
-            {
-                Contract.Assert(ex.Message == "Assertion failed: 'f'");
-                return;
-            }
-            Contract.Throw();
+            Contract.Assert(ex.Message == "Precondition failed: 'f'");
+            return;
         }
-        [TestMethod]
-        public void TestCallerExpressionOnEnsures()
+        Contract.Throw();
+    }
+    [TestMethod]
+    public void TestCallerExpressionOnAssert()
+    {
+        const bool f = false;
+        try
         {
-            const bool f = false;
-            try
-            {
-                Contract.Ensures(f);
-            }
-            catch (Exception ex)
-            {
-                Contract.Assert(ex.Message == "Postcondition failed: 'f'");
-                return;
-            }
-            Contract.Throw();
+            Contract.Assert(f);
         }
+        catch (Exception ex)
+        {
+            Contract.Assert(ex.Message == "Assertion failed: 'f'");
+            return;
+        }
+        Contract.Throw();
+    }
+    [TestMethod]
+    public void TestCallerExpressionOnEnsures()
+    {
+        const bool f = false;
+        try
+        {
+            Contract.Ensures(f);
+        }
+        catch (Exception ex)
+        {
+            Contract.Assert(ex.Message == "Postcondition failed: 'f'");
+            return;
+        }
+        Contract.Throw();
     }
 }
