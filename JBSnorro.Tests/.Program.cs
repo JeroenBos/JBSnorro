80e03e6979467bf3c1a3cb41c39551db115706d7
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2023-01-21 20:53:33 +0100

    Use runSettings in debugging tests(#40)

--- JBSnorro.Tests/.Program.cs
+++ JBSnorro.Tests/.Program.cs
@@ -1,11 +1,16 @@
 ﻿using System.Diagnostics;
-using System.Threading.Tasks;
+using static JBSnorro.Extensions.EnvironmentExtensions;
 
 namespace JBSnorro.Tests
 {
-	class Program
-	{
-		[DebuggerHidden]
-		public static Task Main(string[] args) => Testing.TestExtensions.DefaultMainTestProjectImplementation(args);
-	}
+    class Program
+    {
+        [DebuggerHidden]
+        public static Task Main(string[] args)
+        {
+            RunSettingsUtilities.LoadEnvironmentVariables(runSettingsXmlPath: GetRequiredEnvironmentVariable("RUNSETTINGS_PATH"));
+
+            return Testing.TestExtensions.DefaultMainTestProjectImplementation(args);
+        }
+    }
 }
