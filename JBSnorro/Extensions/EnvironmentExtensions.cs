9276e073ba799f4f56cb4ae3b29581ecbaf9192f
Jeroen Bos <jer123bos@gmail.com>
2023-01-21 20:25:53 +0100

    Simplify

--- JBSnorro/Extensions/EnvironmentExtensions.cs
+++ JBSnorro/Extensions/EnvironmentExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JBSnorro.Extensions;
+﻿namespace JBSnorro.Extensions;
 
 public class EnvironmentExtensions
 {
