6bc1d69bf60b23f558e757bc2b0918b11172adf9
Jeroen Bos <jer123bos@gmail.com>
2023-01-23 01:29:17 +0100

    TDD

---
+++ JBSnorro.Tests/FailingTest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class CrashTest
+{
+    [TestMethod]
+    public void PurposefullyCrashingCI()
+    {
+        throw new Exception();
+    }
+}
