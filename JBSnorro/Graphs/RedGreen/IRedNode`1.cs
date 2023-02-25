16a06b258ec2c359352c16cd53779dd050a8594f
Jeroen Bos <jer123bos@gmail.com>
2023-02-25 21:22:07 +0100

    Refactor

--- JBSnorro/Graphs/RedGreen/IRedNode`1.cs
+++ JBSnorro/Graphs/RedGreen/IRedNode`1.cs
@@ -3,9 +3,9 @@ namespace JBSnorro.Graphs.RedGreen;
 
 public interface IRedNode<TSelf, TGreenNode> where TSelf : class, IRedNode<TSelf, TGreenNode> where TGreenNode : class, IGreenNode<TGreenNode>
 {
-    protected internal TGreenNode Green { get; }
     TSelf? Parent { get; }
     IReadOnlyList<TSelf> Elements { get; }
+    protected internal TGreenNode Green { get; }
     protected internal int IndexInParent { get; }
 
     static virtual TSelf Create(TGreenNode green) => TSelf.Create(green, default, default);
