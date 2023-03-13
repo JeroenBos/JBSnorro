57a9ea85a64b4db2493c1e57d266f8042f6d1c2e
Jeroen Bos <jer123bos@gmail.com>
2023-03-13 13:41:07 +0100

    Format

--- JBSnorro.Tests/.Program.cs
+++ JBSnorro.Tests/.Program.cs
@@ -1,16 +1,15 @@
 ﻿using System.Diagnostics;
 using static JBSnorro.Extensions.EnvironmentExtensions;
 
-namespace JBSnorro.Tests
-{
-    class Program
-    {
-        [DebuggerHidden]
-        public static Task Main(string[] args)
-        {
-            RunSettingsUtilities.LoadEnvironmentVariables(runSettingsXmlPath: GetRequiredEnvironmentVariable("RUNSETTINGS_PATH"));
+namespace JBSnorro.Tests;
 
-            return Testing.TestExtensions.DefaultMainTestProjectImplementation(args);
-        }
+class Program
+{
+    [DebuggerHidden]
+    public static Task Main(string[] args)
+    {
+        RunSettingsUtilities.LoadEnvironmentVariables(runSettingsXmlPath: GetRequiredEnvironmentVariable("RUNSETTINGS_PATH"));
+
+        return Testing.TestExtensions.DefaultMainTestProjectImplementation(args);
     }
 }
