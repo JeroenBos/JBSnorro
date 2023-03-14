6eb43b2931ee18d441160b62df6f2732a48eec4e
Jeroen Bos <jer123bos@gmail.com>
2023-03-14 17:30:20 +0100

    Extend my simple logger

--- JBSnorro/Logging/LogEntry.cs
+++ JBSnorro/Logging/LogEntry.cs
@@ -4,11 +4,13 @@
 	{
 		public string Message { get; }
 		public LogEntryType Type { get; }
+		public DateTime Timestamp { get; }
 
 		public LogEntry(string message, LogEntryType type)
 		{
 			this.Message = message;
 			this.Type = type;
+			this.Timestamp = DateTime.Now;
 		}
 	}
 }
