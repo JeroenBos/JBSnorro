12665bab048125437d3127d3f178f86a701021e0
Jeroen Bos <jer123bos@gmail.com>
2023-03-14 22:37:11 +0100

    Create stub attribute

---
+++ JBSnorro.Testing/IntertestDependency/Inference/DependsOnAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace JBSnorro.Testing.IntertestDependency.Inference;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+sealed class DependsOnAttribute : Attribute
+{
+    public string Method { get; set; }
+    public DependsOnAttribute(string method)
+    {
+        this.Method = method;
+    }
+}
