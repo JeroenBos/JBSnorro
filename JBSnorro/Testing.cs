5deec87637f24774ba6ac5b6182ae647a7d03588
Jeroen Bos <jer123bos@gmail.com>
2023-03-18 14:35:35 +0100

    Refactor

--- JBSnorro/Testing.cs
+++ JBSnorro/Testing.cs
@@ -19,14 +19,9 @@ namespace JBSnorro.Testing
 		}
 		public static void InvokeAllTestMethodsAsynchronouslyIn(this Type testClass, int maxDegreeOfParallelism)
 		{
-
-			Func<MethodInfo, bool> isTestMethodPredicate = methodInfo => methodInfo.GetParameters().Length == 0
-																		 && methodInfo.GetGenericArguments().Length == 0
-																		 && methodInfo.ReturnType == typeof(void)
-																		 && methodInfo.GetCustomAttributes().Any(attribute => attribute.GetType().Name == "TestMethodAttribute");
-
-			Parallel.ForEach(testClass.GetMethods(BindingFlags.Instance | BindingFlags.Public).Where(isTestMethodPredicate), new ParallelOptions() { MaxDegreeOfParallelism = maxDegreeOfParallelism }, // ReSharper disable once PossibleNullReferenceException
-							 testMethod => testMethod.Invoke(testClass.GetConstructor(new Type[0]).Invoke(new object[0]), new object[0]));
+            Parallel.ForEach(TestExtensions.GetTestMethods(testClass), 
+				             new ParallelOptions() { MaxDegreeOfParallelism = maxDegreeOfParallelism },
+							 test => test.Method.Invoke(testClass.GetConstructor(EmptyCollection<Type>.Array).Invoke(EmptyCollection<object>.Array), EmptyCollection<object>.Array));
 		}
 	}
 }
