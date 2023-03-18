9d34983966ec0fa5510c385cb57fbd8b64b6afb1
Jeroen Bos <jer123bos@gmail.com>
2023-03-18 14:35:58 +0100

    Format

--- JBSnorro/Testing.cs
+++ JBSnorro/Testing.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿#nullable enable
 
-namespace JBSnorro.Testing
+namespace JBSnorro.Testing;
+
+public static class TestingExtensions
 {
-	public static class TestingExtensions
+	public static void InvokeAllTestMethodsSynchronouslyIn(this Type testClass)
 	{
-		public static void InvokeAllTestMethodsSynchronouslyIn(this Type testClass)
-		{
-			InvokeAllTestMethodsAsynchronouslyIn(testClass, 1);
-		}
-		public static void InvokeAllTestMethodsAsynchronouslyIn(this Type testClass)
-		{
-			InvokeAllTestMethodsAsynchronouslyIn(testClass, 8);
-		}
-		public static void InvokeAllTestMethodsAsynchronouslyIn(this Type testClass, int maxDegreeOfParallelism)
-		{
+		InvokeAllTestMethodsAsynchronouslyIn(testClass, 1);
+	}
+	public static void InvokeAllTestMethodsAsynchronouslyIn(this Type testClass)
+	{
+		InvokeAllTestMethodsAsynchronouslyIn(testClass, 8);
+	}
+	public static void InvokeAllTestMethodsAsynchronouslyIn(this Type testClass, int maxDegreeOfParallelism)
+	{
             Parallel.ForEach(TestExtensions.GetTestMethods(testClass), 
-				             new ParallelOptions() { MaxDegreeOfParallelism = maxDegreeOfParallelism },
-							 test => test.Method.Invoke(testClass.GetConstructor(EmptyCollection<Type>.Array).Invoke(EmptyCollection<object>.Array), EmptyCollection<object>.Array));
-		}
+			             new ParallelOptions() { MaxDegreeOfParallelism = maxDegreeOfParallelism },
+						 test => test.Method.Invoke(testClass.GetConstructor(EmptyCollection<Type>.Array)!.Invoke(EmptyCollection<object>.Array), EmptyCollection<object>.Array));
 	}
 }
