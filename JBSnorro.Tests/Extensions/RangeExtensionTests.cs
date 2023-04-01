3dc6333d587ed6bbdcdb4758904857c0f38bad1d
Jeroen Bos <jer123bos@gmail.com>
2023-04-01 12:51:23 +0200

    Add RangeExtensions.SubRange(Range, Range)

---
+++ JBSnorro.Tests/Extensions/RangeExtensionTests.cs
@@ -0,0 +1,69 @@
+﻿using JBSnorro.Diagnostics;
+using JBSnorro.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JBSnorro.Tests.Extensions;
+
+[TestClass] 
+public class RangeExtensionTests
+
+{
+    [TestMethod]
+    public void Test_subrange_not_from_end()
+    {
+        var result = RangeExtensions.SubRange(0..10, 1..8);
+        var (offset, length) = result.GetOffsetAndLength(10);
+        Contract.Assert(offset == 1);
+        Contract.Assert(length == 7);
+    }
+
+    [TestMethod]
+    public void Test_subrange_start_from_end()
+    {
+        var result = RangeExtensions.SubRange(0..10, ^7..8);
+        var (offset, length) = result.GetOffsetAndLength(10);
+        Contract.Assert(offset == 3);
+        Contract.Assert(length == 5);
+    }
+
+    [TestMethod]
+    public void Test_subrange_end_from_end()
+    {
+        var result = RangeExtensions.SubRange(0..10, 3..^2);
+        var (offset, length) = result.GetOffsetAndLength(10);
+        Contract.Assert(offset == 3);
+        Contract.Assert(length == 5);
+    }
+    [TestMethod]
+    public void Test_subrange_range_end_from_end()
+    {
+        var result = RangeExtensions.SubRange(0..^1, 3..^2);
+        var (offset, length) = result.GetOffsetAndLength(10);
+        Contract.Assert(offset == 3);
+        Contract.Assert(length == 4);
+    }
+    [TestMethod]
+    public void Test_subrange_range_start_from_end()
+    {
+        var result = RangeExtensions.SubRange(^9..10, 3..^2);
+        var (offset, length) = result.GetOffsetAndLength(10);
+        Contract.Assert(offset == 4);
+        Contract.Assert(length == 4);
+    }
+    [TestMethod]
+    public void Test_subrange_range_end_and_start_from_end()
+    {
+        var result = RangeExtensions.SubRange(0..^1, ^7..8);
+        var (offset, length) = result.GetOffsetAndLength(10);
+        Contract.Assert(offset == 2);
+        Contract.Assert(length == 6);
+    }
+    [TestMethod]
+    public void Test_subrange_range_start_and_start_from_end()
+    {
+        var result = RangeExtensions.SubRange(^9..10, ^6..8);
+        var (offset, length) = result.GetOffsetAndLength(10);
+        Contract.Assert(offset == 4);
+        Contract.Assert(length == 5);
+    }
+}
---
+++ JBSnorro/Extensions/RangeExtensions.cs
@@ -0,0 +1,61 @@
+﻿using JBSnorro.Diagnostics;
+
+namespace JBSnorro.Extensions;
+
+public static class RangeExtensions
+{
+    public static Range SubRange(this Range range, Range subrange)
+    {
+        Index start;
+        if (subrange.Start.IsFromEnd)
+        {
+            if (range.End.IsFromEnd)
+            {
+                start = new Index(subrange.Start.Value + range.End.Value, fromEnd: true);
+            }
+            else
+            {
+                start = range.End.Value - subrange.Start.Value;
+            }
+        }
+        else
+        {
+            if (range.Start.IsFromEnd)
+            {
+                Contract.Requires<ArgumentOutOfRangeException>(range.Start.Value > subrange.Start.Value);
+                start = new Index(range.Start.Value - subrange.Start.Value, fromEnd: true);
+            }
+            else
+            {
+                start = range.Start.Value + subrange.Start.Value;
+            }
+        }
+
+        Index end;
+        if (subrange.End.IsFromEnd)
+        {
+            if (range.End.IsFromEnd)
+            {
+                end = new Index(range.End.Value + subrange.End.Value, fromEnd: true);
+            }
+            else
+            {
+                Contract.Requires<ArgumentOutOfRangeException>(range.End.Value > subrange.End.Value);
+                end = range.End.Value - subrange.End.Value;
+            }
+        }
+        else
+        {
+            if (range.Start.IsFromEnd)
+            {
+                end = new Index(range.Start.Value - subrange.End.Value, fromEnd: true);
+            }
+            else
+            {
+                end = range.Start.Value + subrange.End.Value;
+            }
+        }
+
+        return new Range(start, end);
+    }
+}
