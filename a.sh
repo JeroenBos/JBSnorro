ce7fb0a863d2b32966922c8be45f9db54e31904c
Jeroen Bos <jer123bos@gmail.com>
2023-08-13 19:31:11 +0200

    Works locally from absolute scratch

--- a.sh
+++ a.sh
@@ -1,9 +1,19 @@
 # a script that should succeed:
-TempPackageSource='%TMP%/tmp.eELq5HKfyV'
+TempPackageSource='/tmp/tmp.eELq5HKfyV'
 nuget locals all -clear
+dotnet nuget enable source 'Package source 1' # with 'enable' it works. with 'disable' it doesn't
 
+rm -rf "/tmp/tmp.eELq5HKfyV"
+mkdir -p "/tmp/tmp.eELq5HKfyV"
+rm -rf JBSnorro/bin
+rm -rf JBSnorro/obj
+rm -rf JBSnorro.Tests/bin
+rm -rf JBSnorro.Tests/obj
+rm -rf JBSnorro.Testing/bin
+rm -rf JBSnorro.Testing/obj
+rm -rf JBSnorro.Testing.Tests/bin
+rm -rf JBSnorro.Testing.Tests/obj
 # delete all bins and objs
 
 dotnet publish --configuration Release --framework net7.0 --output "$TempPackageSource" JBSnorro/JBSnorro.csproj
-
 dotnet restore JBSnorro.Testing/JBSnorro.Testing.csproj -p:Configuration=Release
