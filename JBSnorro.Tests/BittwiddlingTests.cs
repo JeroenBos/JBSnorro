dff7ef1d1e4d52a2665e6234fdd3de2f0051983b
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2023-10-13 12:37:16 -0700

    Add `ulong.Xor(bitIndex)` (#89)

---
+++ JBSnorro.Tests/BittwiddlingTests.cs
@@ -0,0 +1,21 @@
+﻿using JBSnorro;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.Contracts;
+
+namespace Tests.JBSnorro;
+
+[TestClass]
+public class BittwiddlingTests
+{
+    [TestMethod]
+    public void TestXor()
+    {
+        var u = 1UL;
+        var v = 2UL;
+
+        Contract.Assert(u.Xor(0) == 0);
+        Contract.Assert(u.Xor(1) == 3);
+        Contract.Assert(v.Xor(0) == 3);
+        Contract.Assert(v.Xor(1) == 0);
+    }
+}
