759225ea92fb11af6b3d3b67f72650f35221782c
Jeroen Bos <jer123bos@gmail.com>
2023-10-15 13:33:11 +0200

    Remove warning

--- JBSnorro/Extensions/RandomExtensions.cs
+++ JBSnorro/Extensions/RandomExtensions.cs
@@ -449,7 +449,7 @@ public static class RandomExtensions
     /// </summary>
     public class SerializableRandomGenerator : IEnumerable<Random>
     {
-        public static JsonConverter<SerializableRandomGenerator> JsonConverter { get; } = new JsonConverterBy2<SerializableRandomGenerator, (int Seed, int Index)>(tuple => new SerializableRandomGenerator(tuple.Seed, tuple.Index), obj => (obj.Seed, obj.CurrentIndex));
+        public static JsonConverter<SerializableRandomGenerator> JsonConverter { get; } = new JsonConverterBy2<SerializableRandomGenerator, (int Seed, int Index)>(tuple => new SerializableRandomGenerator(tuple.Seed, tuple.Index), obj => (obj!.Seed, obj.CurrentIndex));
 
         public int Seed { get; }
         public int CurrentIndex { get; private set; }
