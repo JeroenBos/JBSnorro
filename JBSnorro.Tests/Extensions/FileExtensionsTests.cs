47e31e7aa9b4647e00f48bc1c62dd6e59ccf873a
Jeroen Bos <jer123bos@gmail.com>
2023-10-21 13:53:00 +0200

    Make commit as similar to commit that actually displayed flaky behavior

--- JBSnorro.Tests/Extensions/FileExtensionsTests.cs
+++ JBSnorro.Tests/Extensions/FileExtensionsTests.cs
@@ -1,6 +1,9 @@
-﻿using JBSnorro;
+﻿#pragma warning disable CS0168 // Variable is declared but never used
+
+using JBSnorro;
 using JBSnorro.Diagnostics;
 using JBSnorro.Extensions;
+using JBSnorro.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Diagnostics;
 using TaskExtensions = JBSnorro.Extensions.TaskExtensions;
@@ -10,80 +13,92 @@ namespace Tests.JBSnorro.Extensions;
 [TestClass]
 public class FileExtensionsTests
 {
-    const int step_ms = 500;
+    const int step_ms = 200;
     const int timeout_ms = 10 * step_ms + 5000 /* because in CI it's rather slow */;
-    [TestMethod, Timeout(timeout_ms)]
+    [TestMethod]
+    public async Task TestReadAllLinesContinuouslyAThousandTimes()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.Write(".");
+            try
+            {
+                await TestReadAllLinesContinuously();
+            }
+            catch (Exception ex)
+            {
+                int lineCount = 0;
+                foreach (var writing in FileExtensions.GetWritings())
+                {
+                    lineCount++;
+                    Console.Write(writing);
+                }
+                throw;
+            }
+            finally
+            {
+                FileExtensions.ClearWritings();
+            }
+        }
+    }
     public async Task TestReadAllLinesContinuously()
     {
-        try
+        // Arrange
+        AsyncDisposable<string> pathContainer = IOExtensions.CreateTemporaryFile();
+        var path = pathContainer.Value;
+        Contract.Requires(File.Exists(path));
+
+        var isDone = new Reference<bool>();
+        var readLines = new List<string>();
+        var stopwatch = Stopwatch.StartNew();
+        var readerTask = TaskExtensions.WithTimeout(reader, TimeSpan.FromMilliseconds(timeout_ms));
+        var writerTask = TaskExtensions.WithTimeout(writer, TimeSpan.FromMilliseconds(timeout_ms));
+
+        async Task writer(CancellationToken cancellationToken)
         {
-            // Arrange
-            AsyncDisposable<string> pathContainer = IOExtensions.CreateTemporaryFile();
-            var path = pathContainer.Value;
-            Contract.Requires(File.Exists(path));
-
-            var isDone = new Reference<bool>();
-            var readLines = new List<string>();
-            var stopwatch = Stopwatch.StartNew();
-            var readerTask = TaskExtensions.WithTimeout(reader, TimeSpan.FromMilliseconds(timeout_ms));
-            var writerTask = TaskExtensions.WithTimeout(writer, TimeSpan.FromMilliseconds(timeout_ms));
-
-            async Task writer(CancellationToken cancellationToken)
-            {
-                File.WriteAllLines(path, new string[] { "line 1" });
-                FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Written line 1");
-                await Task.Delay(2 * step_ms);
-                File.AppendAllText(path, "partial line 2. ");
-                FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Written partial line 2");
-                await Task.Delay(4 * step_ms);
-                File.AppendAllLines(path, new string[] { "end of line 2" });
-                FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Written line 2");
-            }
-
-
-            // Act
-            async Task reader(CancellationToken cancellationToken)
-            {
-                await foreach (var line in FileExtensions.ReadAllLinesContinuously(path, isDone, cancellationToken))
-                {
-                    FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} read line");
-                    readLines.Add(line);
-                }
-            }
-
-
-            // Assert
-            await Task.Delay(1 * step_ms);
-            FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Assertion 1");
-            Assert.IsTrue(readLines.SequenceEqual(new string[] {
-                "line 1",
-            }));
-
-            await Task.Delay(3 * step_ms);
-            FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Assertion 2");
-            Assert.IsTrue(readLines.SequenceEqual(new string[] {
-                "line 1",
-            }));
-
-            await Task.Delay(5 * step_ms);
-            FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Assertion 3");
-            Assert.IsTrue(readLines.SequenceEqual(new string[] {
-                "line 1",
-                "partial line 2. end of line 2",
-            }));
-
-            isDone.Value = true;
+            File.WriteAllLines(path, new string[] { "line 1" });
+            FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Written line 1");
+            await Task.Delay(2 * step_ms);
+            File.AppendAllText(path, "partial line 2. ");
+            FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Written partial line 2");
+            await Task.Delay(4 * step_ms);
+            File.AppendAllLines(path, new string[] { "end of line 2" });
+            FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Written line 2");
         }
-        catch
+
+
+        // Act
+        async Task reader(CancellationToken cancellationToken)
         {
-            foreach (var writing in FileExtensions.GetWritings())
+            await foreach (var line in FileExtensions.ReadAllLinesContinuously(path, isDone, cancellationToken))
             {
-                Console.Write(writing);
+                FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} read line");
+                readLines.Add(line);
             }
         }
-        finally
-        {
-            FileExtensions.ClearWritings();
-        }
+
+
+        // Assert
+        await Task.Delay(1 * step_ms);
+        FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Assertion 1");
+        Assert.IsTrue(readLines.SequenceEqual(new string[] {
+            "line 1",
+        }));
+
+        await Task.Delay(3 * step_ms);
+        FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Assertion 2");
+        Assert.IsTrue(readLines.SequenceEqual(new string[] {
+            "line 1",
+        }));
+
+        await Task.Delay(5 * step_ms);
+        FileExtensions.WriteLine($"{stopwatch.ElapsedMilliseconds} Assertion 3");
+        Assert.IsTrue(readLines.SequenceEqual(new string[] {
+            "line 1",
+            "partial line 2. end of line 2",
+        }));
+
+        isDone.Value = true;
+
     }
 }
--- JBSnorro/Extensions/FileExtensions.cs
+++ JBSnorro/Extensions/FileExtensions.cs
@@ -29,13 +29,24 @@ public static class FileExtensions
             list.Clear();
         }
     }
+    public static IAsyncEnumerable<string> ReadAllLinesContinuously(string path, Reference<bool>? done = null, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            return _ReadAllLinesContinuously(path, done, cancellationToken);
+        }
+        finally
+        {
+            WriteLine("looping exit");
+        }
+    }
     /// <summary>
     /// Continuously reads all lines of a file and yields are lines written to it by other processes.
     /// </summary>
     /// <param name="path">The path of the file to read.</param>
     /// <param name="done">A boolean indicating whether we can stop reading all lines.</param>
     /// <param name="cancellationToken">A cancellation token for regular throw-on-canceled use.</param>
-    public static async IAsyncEnumerable<string> ReadAllLinesContinuously(
+    public static async IAsyncEnumerable<string> _ReadAllLinesContinuously(
         string path,
         Reference<bool>? done = null,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
@@ -47,16 +58,17 @@ public static class FileExtensions
 
         IAsyncEnumerable<object?> everyFileChange = IAsyncEnumerableExtensions.Create(out var yield, out var dispose);
         string? error = null;
-        var watcher = new FileSystemWatcher(Path.GetDirectoryName(path)!, Path.GetFileName(path))
-        {
-            EnableRaisingEvents = true,
-        };
+        var watcher = new FileSystemWatcher(Path.GetDirectoryName(path)!, Path.GetFileName(path));
         watcher.Changed += (sender, e) => { WriteLine("yielding ping"); yield(); WriteLine("yielding pong"); };
         watcher.Error += (sender, e) => { error = "error"; dispose(); };
         watcher.Deleted += (sender, e) => { error = "deleted"; dispose(); };
         watcher.Disposed += (sender, e) => { error = "disposed"; dispose(); };
         watcher.Renamed += (sender, e) => { error = "renamed"; dispose(); };
 
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        // HACK: I have absolutely no clue, but if I set EnableRaisingEvents to true in a task, it seems to work; otherwise it doesn't. I'm suspecting a deadlock somewhere but I can't find it.
+        Task.Run(async () => { await Task.Delay(1); watcher.EnableRaisingEvents = true; }, cancellationToken);
+#pragma warning restore CS4014
 
         done ??= new Reference<bool>();
         var streamPosition = new Reference<long>();
@@ -121,18 +133,72 @@ public static class FileExtensions
         fs.Position = readStreamPosition.Value;
         long stringBuilderStartPosition = -1;
         StringBuilder? stringBuilder = null;
-        try
+        while (true)
         {
-            while (true)
+            WriteLine("looping");
+            long streamPositionBeforeCurrentRead = sr.BaseStream.Position;
+            string? line;
+            try
             {
-                WriteLine("looping");
-                long streamPositionBeforeCurrentRead = sr.BaseStream.Position;
-                string? line;
+                WriteLine("before sr.ReadLineAsync");
+                line = await sr.ReadLineAsync(cancellationToken);
+                WriteLine("after sr.ReadLineAsync");
+            }
+            catch (TaskCanceledException)
+            {
+                if (done.Value == true)
+                    break;
+                else
+                    throw;
+            }
+            finally
+            {
+                readStreamPosition.Value = sr.BaseStream.Position;
+            }
+            if (!string.IsNullOrEmpty(line))
+            {
+                if (sr.LastCharacterWasNewLine)
+                {
+                    if (stringBuilder == null)
+                    {
+                        WriteLine("Yielding line a");
+                        yield return line;
+                        WriteLine("Yielded line a");
+                    }
+                    else
+                    {
+                        stringBuilder.Append(line);
+                        WriteLine("Yielding line b");
+                        yield return stringBuilder.ToString();
+                        WriteLine("Yielded line b");
+                        stringBuilder = null;
+                        stringBuilderStartPosition = -1;
+                    }
+                }
+                else
+                {
+                    stringBuilder ??= new StringBuilder();
+                    stringBuilderStartPosition = streamPositionBeforeCurrentRead;
+                    stringBuilder.Append(line);
+                }
+            }
+            else if (line == null)
+            {
+                if (stringBuilder != null)
+                {
+                    readStreamPosition.Value = stringBuilderStartPosition;
+                }
+                yield break;
+            }
+            else if (done.Value == true)
+            {
+                break;
+            }
+            else
+            {
                 try
                 {
-                    WriteLine("before sr.ReadLineAsync");
-                    line = await sr.ReadLineAsync(cancellationToken);
-                    WriteLine("after sr.ReadLineAsync");
+                    await Task.Delay(10, cancellationToken);
                 }
                 catch (TaskCanceledException)
                 {
@@ -141,68 +207,7 @@ public static class FileExtensions
                     else
                         throw;
                 }
-                finally
-                {
-                    readStreamPosition.Value = sr.BaseStream.Position;
-                }
-                if (!string.IsNullOrEmpty(line))
-                {
-                    if (sr.LastCharacterWasNewLine)
-                    {
-                        if (stringBuilder == null)
-                        {
-                            WriteLine("Yielding line a");
-                            yield return line;
-                            WriteLine("Yielded line a");
-                        }
-                        else
-                        {
-                            stringBuilder.Append(line);
-                            WriteLine("Yielding line b");
-                            yield return stringBuilder.ToString();
-                            WriteLine("Yielded line b");
-                            stringBuilder = null;
-                            stringBuilderStartPosition = -1;
-                        }
-                    }
-                    else
-                    {
-                        stringBuilder ??= new StringBuilder();
-                        stringBuilderStartPosition = streamPositionBeforeCurrentRead;
-                        stringBuilder.Append(line);
-                    }
-                }
-                else if (line == null)
-                {
-                    if (stringBuilder != null)
-                    {
-                        readStreamPosition.Value = stringBuilderStartPosition;
-                    }
-                    yield break;
-                }
-                else if (done.Value == true)
-                {
-                    break;
-                }
-                else
-                {
-                    try
-                    {
-                        await Task.Delay(10, cancellationToken);
-                    }
-                    catch (TaskCanceledException)
-                    {
-                        if (done.Value == true)
-                            break;
-                        else
-                            throw;
-                    }
-                }
             }
-        }
-        finally
-        {
-            WriteLine("looping exit");
         }
     }
 }
