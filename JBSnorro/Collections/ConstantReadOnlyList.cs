5512735ae3da2ea7347f5adb5afd5ddbbf5c2d7e
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2023-10-29 22:00:11 +0100

    Create `ConstantReadOnlyList` (#100)

---
+++ JBSnorro/Collections/ConstantReadOnlyList.cs
@@ -0,0 +1,38 @@
+﻿using JBSnorro.Diagnostics;
+using System.Collections;
+
+namespace JBSnorro.Collections;
+
+/// <summary>
+/// Represents an immutable list consisting of an element any number of times.
+/// </summary>
+public class ConstantReadOnlyList<T> : IReadOnlyList<T>
+{
+    public T Value { get; }
+    public int Count { get; }
+
+    public ConstantReadOnlyList(T value, int count)
+    {
+        Contract.Requires(count >= 0);
+
+        this.Value = value;
+        this.Count = count;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            Contract.Requires<IndexOutOfRangeException>(index >= 0);
+            Contract.Requires<IndexOutOfRangeException>(index < Count);
+            return this.Value;
+        }
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        return Enumerable.Range(0, this.Count)
+                         .Select(_ => Value)
+                         .GetEnumerator();
+    }
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
